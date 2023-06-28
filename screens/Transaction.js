7beb878ef1b1266a753b742bcf102e8c18187ebf
koinskii <andrekoinski8@gmail.com>
2023-06-27 21:20:16 -0300

    leitor qr

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,14 +1,54 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
+import * as Permissions from 'expo-permissions'
+import { BarCodeScanner } from "expo-barcode-scanner"
+export default class Transaction extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            domState: 'normal',
+            hasCameraPermissons: null,
+            scanned: false,
+            scannedData: "",
+        }
+    }
 
-export default class Transaction extends React.Component{
-    render(){
+    getCameraPermissions = async (domState) => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA)
+        this.setState({
+            hasCameraPermissons: status == 'granted',
+            domState: domState,
+            scanned: false,
+        })
+    }
+
+    handleBarCodeScanned = async ({type, data})=>{
+        this.setState({
+            scannedData: data,
+            domState: 'normal',
+            scanned: true,
+        })
+    }
+
+    render() {
+        const { domState, hasCameraPermissons, scanned, scannedData } = this.state
+
+        if (domState == 'scanner') {
+            return(
+                <BarCodeScanner
+                onBarCodeScanned={scanned? undefined : this.handleBarCodeScanned}
+                style = {StyleSheet.absoluteFillObject}
+                />
+            )
+        }
         return (
-            <View style = {styles.Container}>
-                <Text style = {styles.text}>
-                    Tela de transação
-
+            <View style={styles.Container}>
+                <Text style={styles.text}>
+                    {hasCameraPermissons? scannedData : "Permita o uso da camera"}
                 </Text>
+                <TouchableOpacity onPress={() => this.getCameraPermissions("scanner")} style={ styles.button}>
+                    <Text style={styles.buttonText}>Leitor QR</Text>
+                </TouchableOpacity>
 
             </View>
         )
@@ -17,14 +57,26 @@ export default class Transaction extends React.Component{
 
 const styles = StyleSheet.create({
     Container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5653D4',
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#5653D4"
     },
     text: {
-        color: 'white',
-        fontSize: 30,
+      color: "#ffff",
+      fontSize: 15
+    },
+    button: {
+      width: "43%",
+      height: 55,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#F48D20",
+      borderRadius: 15
+    },
+    buttonText: {
+      fontSize: 15,
+      color: "#FFFFFF"
     }
-})
-
+   
+  });
