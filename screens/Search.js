06ad46b0b7b0d3e3d357f05e062e46da76ff8a6f
koinskii <andrekoinski8@gmail.com>
2023-07-24 13:50:52 -0300

    pesquisa

--- screens/Search.js
+++ screens/Search.js
@@ -1,14 +1,158 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, TextInput, } from "react-native"
+import db from "../config"
+import firebase from 'firebase'
+import { Avatar, ListItem, Icon } from "react-native-elements"
 
-export default class Search extends React.Component{
-    render(){
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            allTransactions: [],
+            searchText: '',
+            lastVisibleTransaction: '',
+
+        }
+    }
+    componentDidMount() {
+        this.getTransaction()
+    }
+
+    getTransaction = () => {
+        db.collection('transactions').get().then(
+            snapshot => {
+                snapshot.docs.map(doc => {
+                    this.setState({
+                        allTransactions: [...this.state.allTransactions, doc.data()]
+                    })
+                })
+            }
+        )
+    }
+
+    renderItem = ({ item, i }) => {
+        var date = item.date
+            .toDate()
+            .toString()
+            .split(" ")
+            .splice(0, 4)
+            .join(" ");
+
+        var transactionType =
+            item.transaction_type === "issue" ? "entregue" : "devolvido";
         return (
-            <View style = {styles.Container}>
-                <Text style = {styles.text}>
-                    Tela de Pesquisa
+            <View style={{ borderWidth: 1 }}>
+                <ListItem key={i} bottomDivider>
+                    <Icon type={"antdesign"} name={"book"} size={40} />
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.title}>
+                            {`${item.book_name} ( ${item.book_id} )`}
+                        </ListItem.Title>
+                        <ListItem.Subtitle style={styles.subtitle}>
+                            {`Este livro foi ${transactionType} por ${item.student_name}`}
+                        </ListItem.Subtitle>
+                        <View style={styles.lowerLeftContaiiner}>
+                            <View style={styles.transactionContainer}>
+                                <Text
+                                    style={[
+                                        styles.transactionText,
+                                        {
+                                            color:
+                                                item.transaction_type === "issue"
+                                                    ? "#78D304"
+                                                    : "#0364F4"
+                                        }
+                                    ]}
+                                >
+                                    {item.transaction_type.charAt(0).toUpperCase() +
+                                        item.transaction_type.slice(1)}
+                                </Text>
+                                <Icon
+                                    type={"ionicon"}
+                                    name={
+                                        item.transaction_type === "issue"
+                                            ? "checkmark-circle-outline"
+                                            : "arrow-redo-circle-outline"
+                                    }
+                                    color={
+                                        item.transaction_type === "issue" ? "#78D304" : "#0364F4"
+                                    }
+                                />
+                            </View>
+                            <Text style={styles.date}>{date}</Text>
+                        </View>
+                    </ListItem.Content>
+                </ListItem>
+            </View>
+        );
+    };
 
-                </Text>
+    //realizando uma busca
+    handleSearch = async text => {
+        var enteredText = text.toUpperCase().split("");//convertendo o texto em UPPERCASE e dividindo as letras em um array
+        text = text.toLowerCase(); //convertendo o texto em lowerCase
+        this.setState({
+            allTransactions: [] //zerando as transações
+        });
+        if (!text) {
+            this.getTransactions(); //se o texto estiver em branco chamo as transações
+        }
+
+        if (enteredText[0] === "B") { //se o texto começar como B busco pelo BookId
+            db.collection("transactions")
+                .where("book_id", "==", text)
+                .get()
+                .then(snapshot => {
+                    snapshot.docs.map(doc => {
+                        this.setState({
+                            allTransactions: [...this.state.allTransactions, doc.data()],//Jogo no array todas as transações com o livro em questão
+                            lastVisibleTransaction: doc
+                        });
+                    });
+                });
+        } else if (enteredText[0] === "S") { //se o texto começar com S busco pelo StudentId
+            db.collection("transactions")
+                .where("student_id", "==", text)
+                .get()
+                .then(snapshot => {
+                    snapshot.docs.map(doc => {
+                        this.setState({
+                            allTransactions: [...this.state.allTransactions, doc.data()],//Jogo no array todas as transações com o estudante em questão
+                            lastVisibleTransaction: doc
+                        });
+                    });
+                });
+        }
+    };
+
+
+    render() {
+        const { allTransactions, searchText } = this.state
+        return (
+            <View style={styles.container}>
+                <View style={styles.upperContainer}>
+                    <View style={styles.textinputContainer}>
+                        <TextInput style={styles.textinput}
+                            onChangeText={text => { this.setState({ searchText: text }) }}
+                            placeholder="Digite Aqui"
+                            placeholderTextColor={"#fff"}
+                        />
+                        <TouchableOpacity style={styles.scanbutton} 
+                        onPress={()=>{this.handleSearch(searchText)}}>
+                            <Text style={styles.scanbuttonText}>
+                                Consultar
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.lowerContainer}>
+                    <FlatList
+                        data={allTransactions}
+                        renderItem={this.renderItem}
+                        keyExtractor={(item, index) => { index.toString() }}
+                    />
+
+                </View>
 
             </View>
         )
@@ -16,15 +160,72 @@ export default class Search extends React.Component{
 }
 
 const styles = StyleSheet.create({
-    Container: {
+    container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5653D4',
+        backgroundColor: "#5653D4"
     },
-    text: {
-        color: 'white',
-        fontSize: 30,
+    upperContainer: {
+        flex: 0.2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textinputContainer: {
+        borderWidth: 2,
+        borderRadius: 10,
+        flexDirection: "row",
+        backgroundColor: "#9DFD24",
+        borderColor: "#FFFFFF"
+    },
+    textinput: {
+        width: "57%",
+        height: 50,
+        padding: 10,
+        borderColor: "#FFFFFF",
+        borderRadius: 10,
+        borderWidth: 3,
+        fontSize: 18,
+        backgroundColor: "#5653D4",
+        color: "#FFFFFF"
+    },
+    scanbutton: {
+        width: 100,
+        height: 50,
+        backgroundColor: "#9DFD24",
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    scanbuttonText: {
+        fontSize: 20,
+        color: "#0A0101",
+    },
+    lowerContainer: {
+        flex: 0.8,
+        backgroundColor: "#FFFFFF"
+    },
+    title: {
+        fontSize: 20,
+    },
+    subtitle: {
+        fontSize: 16,
+    },
+    lowerLeftContaiiner: {
+        alignSelf: "flex-end",
+        marginTop: -40
+    },
+    transactionContainer: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center"
+    },
+    transactionText: {
+        fontSize: 20,
+
+    },
+    date: {
+        fontSize: 12,
+        paddingTop: 5
     }
-})
+});
 
