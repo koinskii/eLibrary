6dab4bbfc3130599b4b8a46aa122297b93ba3c43
koinskii <andrekoinski8@gmail.com>
2023-07-27 21:24:26 -0300

    erro :((

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import TabNavigator from './navigation/TabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/login';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <TabNavigator/>
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
 
-    </NavigationContainer>
-  );
-}
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return(
+        <NavigationContainer>
+          <AppContainer/>
+        </NavigationContainer>
+      )
+    }
+    return null;
+  }}
+const AppSwitchNavigator = createSwitchNavigator({
+  Login:{screen: Login},
+  BottonTab:{screen: TabNavigator}
+
+},
+{initialRouteName:'Login'}
+)
+
+const AppContainer = createAppContainer(AppSwitchNavigator)
